41f36ecdb1204afa1559a020a145ab1d391534ac
DavidBak <sejongbgy07@gmail.com>
2021-03-23 22:37:44 +0900

    completed

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -52,7 +52,8 @@ function LandingPage() {
                 <React.Fragment key={index}>
                     
                     
-                    <GridCard 
+                    <GridCard
+                        landingPage
                         image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
                         movieId={movie.id}
                         movieName={movie.original_title}
--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -3,15 +3,30 @@ import {Col} from 'antd';
 
 
 function GridCard(props) {
-    return (
-        <Col  md={6} sm={8} xs={24}>
-        <div style={{position: 'relative'}}>
-            <a href={`/movie/${props.movieId}`}>
-                <img style={{width:'100%', height: ''}} src={props.image} alt={props.movieName}/>
-            </a>
-        </div>
-       </Col>
-    )
+
+    if(props.landingPage){
+        return (
+            <Col  md={6} sm={8} xs={24}>
+            <div style={{position: 'relative'}}>
+                <a href={`/movie/${props.movieId}`}>
+                    <img style={{width:'100%', height: ''}} src={props.image} alt={props.movieName}/>
+                </a>
+            </div>
+           </Col>
+        )
+    } else{
+        return (
+            <Col  md={6} sm={8} xs={24}>
+            <div style={{position: 'relative'}}>
+                 <img style={{width:'100%', height: ''}} src={props.image} alt={props.characterName}/>
+            </div>
+           </Col>
+        )
+    }
+
+
+
+   
 }
 
 export default GridCard
